dad1a293a34b3e0f9e2395f4aefe87430ac4358a
thomas mayrhofer <thomas@mayrhofer.at>
2013-11-10 16:45:55 +0100

    created rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoodMesh::Application.config.secret_key_base = 'c76f78c874cf7ea4aa877712b3b554e71f35a2ef7185dd6e5a465d5228ac3cfe15e13ed064ee651fcfc21f1ebbb5f3964da849953f97ddb1fcbe244b1a766c47'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FoodMesh::Application.config.session_store :cookie_store, key: '_food-mesh_session'
