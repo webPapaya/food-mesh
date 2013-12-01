0ee674369b8e4df5135e42e4c2cdfff9270563db
franzi <foberhauser.mmt-b2012@fh-salzburg.ac.at>
2013-12-02 00:39:44 +0100

    added markdown editor: redcarpet

--- app/helpers/basic_pages_helper.rb
+++ app/helpers/basic_pages_helper.rb
@@ -1,2 +1,16 @@
 module BasicPagesHelper
+  # see if we just use it in basic_page helper
+  def markdown(text)
+    options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
+    renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true)
+    raw renderer.render(text)
+  end
+
+  def syntax_highlighter(html)
+    doc = Nokogiri::HTML(html)
+    doc.search("//pre[@lang]").each do |pre|
+      pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
+    end
+    doc.to_s
+  end
 end
