b20204563aa950d4a39563c5ccb6ad2bf30ee5d7
thomas mayrhofer <thomas@mayrhofer.at>
2013-12-06 21:54:03 +0100

    added parse function to fddb

--- app/models/pie_chart.rb
+++ app/models/pie_chart.rb
@@ -87,7 +87,7 @@ end
 ####################
 
 module FoodAPIs
-  @@apis = [Fsecret.new, Fddb.new];
+  @@apis = [Fddb.new];
 
   def search(query)
     result = ""
@@ -142,12 +142,40 @@ end
 
 class Fddb
   def initialize
-
+    @api_key = 'U9H3TXH05S933NMQFMJIL64C'
   end
 
+  def parse_xml (data)
+    object = Array.new
+    xmlObj = Nokogiri::XML(data)
+
+    #xmlObj.xpath("//item/data//*[not(text())]").remove
+    #
+    #xmlObj.xpath("//item/data").each do |item|
+    #  item.xpath("//*").select{|n| n.inner_text}.each do |ingredient|
+    #    if ingredient.empty?
+    #      name = ingredient.name
+    #      amount = ingredient.content
+    #      puts amount
+    #    end
+    #  end
+    #end
+
+    object.to_s
+  end
 
   def search query
-    "fddb #{query} "
+    params = {
+        :apikey => @api_key,
+        :q => query,
+        :lang => 'de'
+    }
+
+    data = Curl::Easy.http_post('http://fddb.info/api/v8/search/item.xml?' + params.to_query).body_str
+
+
+
+    parse_xml data
   end
 end
 
