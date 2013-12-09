429e5783b7ce1b26d6b23da39660492fc470e3c6
franzi <foberhauser.mmt-b2012@fh-salzburg.ac.at>
2013-12-09 13:03:05 +0100

    added translations

--- app/models/food_apis/fsecret.rb
+++ app/models/food_apis/fsecret.rb
@@ -32,7 +32,7 @@ class Fsecret
       ingredients = desc.split(" | ")
       ingredients.each do |ingredient|
         tmp = ingredient.split(": ")
-        key = key = I18n.t tmp[0], locale: :fatsecret
+        key  = I18n.t tmp[0], locale: :fatsecret
         food["nutritions"][key] = tmp[1]
       end
       parsed_data.push(food)
