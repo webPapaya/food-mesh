b5c188a66b28b0a87bd518aa06fbf766003be158
franzi <foberhauser.mmt-b2012@fh-salzburg.ac.at>
2013-12-13 23:22:38 +0100

    hash tabele with index for key value pair (ingredient, amount)

--- app/controllers/line_charts_controller.rb
+++ app/controllers/line_charts_controller.rb
@@ -7,27 +7,27 @@ class LineChartsController < ApplicationController
     @start_x = 0
     @start_y = 500
 
-    values = {
-        "value1" => 202,
-        "value2" => 94,
-        "value3" => 58,
-        "value4" => 58,
-        "value5" => 58,
-        "value6" => 208
+    @values = {
+        "value1" => 200,
+        "value2" => 940,
+        "value3" => 580,
+        "value4" => 580,
+        "value5" => 80,
+        "value6" => 200
     }
 
 
     # base line of every triangle
-    @base_line = @window_width/values.length
+    @base_line = @window_width/@values.length
 
-    array = [400, 300, 100,  250, 50,  400]
+    #array = [400, 300, 100,  250, 50,  400]
 
     # hash with name of
-    values.each do|name,value|
-      puts "#{name}: #{value}"
+    @values.each_with_index do|(name, value), index|
 
       @draw_x =  @base_line*2
       @draw_y = @start_y - value
+      @variable = "#{value} => #{index}"
 
     end
 
