85733aea57f4767c08f6d8161b673ab0fdaa9884
franzi <foberhauser.mmt-b2012@fh-salzburg.ac.at>
2013-12-13 21:55:20 +0100

    line drawn with last value of key value pair from hash

--- app/controllers/line_charts_controller.rb
+++ app/controllers/line_charts_controller.rb
@@ -1,45 +1,35 @@
 class LineChartsController < ApplicationController
 
-  # GET /basic_pages
-  # GET /basic_pages.json
   def index
     @window_width= 1000
-    @window_height= 1000
+    @window_height = 1000
 
     @start_x = 0
     @start_y = 500
 
-    # assumed values coming from the database
-    @first_db_value = 400   # example 400 calories
-    @second_db_value = 300
-    @fourth_db_value = 100
-
-    @data_amount = 6  # example 6 different values from database
-
+    values = {
+        "value1" => 202,
+        "value2" => 94,
+        "value3" => 58,
+        "value4" => 58,
+        "value5" => 58,
+        "value6" => 208
+    }
 
 
     # base line of every triangle
-    @base_line = @window_width/@data_amount
+    @base_line = @window_width/values.length
 
-    #@path_length = (Math.sqrt(((@base_line/2)**2) * (@db_value**2))).ceil
+    array = [400, 300, 100,  250, 50,  400]
 
+    # hash with name of
+    values.each do|name,value|
+      puts "#{name}: #{value}"
 
-    @second_x = @base_line/2
-    @second_y = @start_y - @first_db_value
-    @third_x =  @base_line #@base_line/4
-    @third_y =  Math.sqrt((@start_y - @first_db_value - @second_db_value)**2)
-    @fourth_x = @base_line+@base_line/2  #wrong calculatun -> x-values alwas get added
-    @fouth_y = @start_y - @fourth_db_value
-  end
+      @draw_x =  @base_line*2
+      @draw_y = @start_y - value
 
-  def init
-    #@start_x = 100
-    #@start_y = 500
-  end
+    end
 
-  def draw
-    #@second_x = 500
-    #@second_y = 0
-    #@third_x =
   end
 end
