589a394a4e9dc6110aa5c9b24a4e543f17766389
franzi <foberhauser.mmt-b2012@fh-salzburg.ac.at>
2013-12-14 23:56:22 +0100

    fixed bug in line chart

--- app/controllers/line_charts_controller.rb
+++ app/controllers/line_charts_controller.rb
@@ -1,19 +1,23 @@
 class LineChartsController < ApplicationController
 
   def index
-    @window_width= 1000
-    @window_height = 1000
+    @window_width = 1000
+    @window_height = 500
 
-    @start_x = 0
-    @start_y = 900
+    #@start_x = 0
+    #@start_y = 900
 
     @values = {
-        "value1" => 400,
-        "value2" => 140,
-        "value3" => 580,
-        "value4" => 180,
-        "value5" => 80,
-        "value6" => 300
+        "value1" => 0,
+        "value2" => 400,
+        "value3" => 0,
+        "value4" => 400,
+        "value5" => 0,
+        "value6" => 400,
+        "value7" => 0,
+        "value8" => 400,
+        "value9" => 0,
+        "value10" => 400
     }
 
     # base line of every triangle
@@ -30,12 +34,12 @@ class LineChartsController < ApplicationController
 
       logger.debug"#{name}, #{value} => #{index}"
 
-      logger.debug@x2[index] =  (@base_line/2) * (index + 1)
-      logger.debug@y2[index] = @start_y - value
+      logger.debug@x2[index] =  (@base_line) * (index)
+      logger.debug@y2[index] = @window_height - value
 
       if index == 0
-        logger.debug@x1[index] = @start_x
-        logger.debug@y1[index] = @start_y
+        logger.debug@x1[index] = 0
+        logger.debug@y1[index] =  @window_height - value
       else
         logger.debug@x1[index] = @x2[index - 1]
         logger.debug@y1[index] = @y2[index - 1]
