217043d012a350c2f7644e00785ce92c7d029c02
thomas mayrhofer <thomas@mayrhofer.at>
2013-12-22 16:53:00 +0100

    removed log message

--- app/controllers/user_session_controller.rb
+++ app/controllers/user_session_controller.rb
@@ -3,8 +3,6 @@ class UserSessionController < ApplicationController
   def change_user_settings
     user_session.change_user_settings params
 
-    ap user_session.get_user_settings
-
     respond_to do |format|
       format.html { redirect_to :back}
       format.js
