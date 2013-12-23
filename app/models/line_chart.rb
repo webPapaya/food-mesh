d93a1cc8132db33f34642e7149b91d8e37f2b37a
franzi <foberhauser.mmt-b2012@fh-salzburg.ac.at>
2013-12-23 02:34:13 +0100

    proper line chart

--- app/models/line_chart.rb
+++ app/models/line_chart.rb
@@ -2,9 +2,8 @@ require_dependency 'food_apis_module'
 
 class LineChart < ActiveRecord::Base
 
-  def initialize
-    @window_height = 500
-    @window_width = 1000
+
+  def initialize(window_width = 500, window_height = 1000)
     @values = {
         "fat" => 100,
         "calories" => 200,
@@ -17,6 +16,8 @@ class LineChart < ActiveRecord::Base
         "vitamin_a" => 290,
         "iron" => 300,
     }
+    @window_height = window_width
+    @window_width = window_height
   end
 
 
@@ -30,33 +31,6 @@ class LineChart < ActiveRecord::Base
   end
 
 
-  def get_values
-
-    @value = {
-        "fat" => 100,
-        "calories" => 200,
-        "protein" => 490,
-        "calcium" => 300,
-        "sugar" => 310,
-        "water" => 390,
-        "alcohol" => 250,
-        "vitamin_c" => 300,
-        "vitamin_a" => 290,
-        "iron" => 300,
-    }
-    #[{
-    #     "name"=>"Tilapia (Fish)",
-    #     "amount"=>"Per 100g",
-    #     "object_source_id" => 70157557973400,   # no comma and arrow
-    #     "nutritions"=>{
-    #         "Calories"=>"96kcal",
-    #         "Fat"=>"1.70g",
-    #         "Carbs"=>"0.00g",
-    #         "Protein"=>"20.08g"
-    #     }
-    # }]
-
-  end
 
   def draw_function
 
