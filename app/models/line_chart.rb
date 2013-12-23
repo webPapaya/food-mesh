df5e5431ae564553cb03d7af761fa3e0a102a137
franzi <foberhauser.mmt-b2012@fh-salzburg.ac.at>
2013-12-23 02:21:31 +0100

    line chart in model line_chart.rb

--- app/models/line_chart.rb
+++ app/models/line_chart.rb
@@ -3,18 +3,8 @@ require_dependency 'food_apis_module'
 class LineChart < ActiveRecord::Base
 
   def initialize
-    @window_width = 1000
     @window_height = 500
-  end
-
-  def get_line_chart
-    {
-      :values => get_values
-    }
-  end
-
-
-  def get_values
+    @window_width = 1000
     @values = {
         "fat" => 100,
         "calories" => 200,
@@ -27,29 +17,50 @@ class LineChart < ActiveRecord::Base
         "vitamin_a" => 290,
         "iron" => 300,
     }
+  end
+
+
+  def get_line_chart
+    {
+      :init => initialize,
+      #:values => get_values,
+      :draw => draw_function
+
+     }
+  end
+
+
+  def get_values
+
+    @value = {
+        "fat" => 100,
+        "calories" => 200,
+        "protein" => 490,
+        "calcium" => 300,
+        "sugar" => 310,
+        "water" => 390,
+        "alcohol" => 250,
+        "vitamin_c" => 300,
+        "vitamin_a" => 290,
+        "iron" => 300,
+    }
+    #[{
+    #     "name"=>"Tilapia (Fish)",
+    #     "amount"=>"Per 100g",
+    #     "object_source_id" => 70157557973400,   # no comma and arrow
+    #     "nutritions"=>{
+    #         "Calories"=>"96kcal",
+    #         "Fat"=>"1.70g",
+    #         "Carbs"=>"0.00g",
+    #         "Protein"=>"20.08g"
+    #     }
+    # }]
 
   end
 
-  def get_db_values
-    #@start_x = 0
-    #@start_y = 900
+  def draw_function
 
 
-    [{
-         "name"=>"Tilapia (Fish)",
-         "amount"=>"Per 100g",
-         "object_source_id" => 70157557973400,   # no comma and arrow
-         "nutritions"=>{
-             "Calories"=>"96kcal",
-             "Fat"=>"1.70g",
-             "Carbs"=>"0.00g",
-             "Protein"=>"20.08g"
-         }
-     }]
-
-    #logger.debug("FoodApisModule")
-    #logger.debug(FoodApisModule.@@apis())
-    #get_item()
 
     @path = "M 0 #{@window_height}"
     i = 0
