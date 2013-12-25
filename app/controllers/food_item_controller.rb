4c95d9a5f209c77ece25240f5bef4f4cbb453adb
thomas mayrhofer <thomas@mayrhofer.at>
2013-12-25 23:56:08 +0100

    added first mongoDB insert into db

--- app/controllers/food_item_controller.rb
+++ app/controllers/food_item_controller.rb
@@ -1,5 +1,15 @@
+require 'food_apis_module'
+
 class FoodItemController < ApplicationController
+  include FoodApisModule
+
+  #def index
+  #  #@food_item = FoodItem.new_item
+  #end
+
   def index
-    @food_item = FoodItem.new_item
+    item = get_item 0, '513fceb675b8dbbc21001d62'
+    @food_item = FoodItem.new_item item
+
   end
 end
--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,11 +1,18 @@
+require 'food_apis_module'
+
 class FoodItem
   include Mongoid::Document
+  include FoodApisModule #include all functions from foodAPIs Module
 
   field :name, type: String
   field :nutritions, type: Object
   field :search, type: Array
 
-  def self.new_item
-    "test"
+  def self.new_item item
+    name = item[:name]
+    nutritions = item[:nutritions]
+    i = FoodItem.new(name: name, nutritions: nutritions, search: [])
+    i.save
+    i
   end
 end
