28d5ab10f09ce1a8f43d41f4a1e452dae82d556d
thomas mayrhofer <thomas@mayrhofer.at>
2013-12-25 20:49:52 +0100

    refactored

--- app/models/line_chart.rb
+++ app/models/line_chart.rb
@@ -1,28 +1,25 @@
-require_dependency 'food_apis_module'
-
 class LineChart < ActiveRecord::Base
-  def initialize(window_width = 500, window_height = 1000)
-    @window_height = window_width
-    @window_width = window_height
+  def initialize(window_width = 1000, window_height = 500)
+    @dimensions = {
+        :width => window_width,
+        :height => window_height
+    }
   end
 
   def build_paths(items)
     items.each do |item|
       item[:path] = draw_function item[:nutritions]
     end
-    items
   end
 
   def draw_function(nutritions)
-    path = "M 0 #{500}"
     space = 500/nutritions.length
-
+    path = "M 0 #{@dimensions[:height]}"
     i = 0
     nutritions.each do |key, value|
-      path << " L #{i*space}  #{500-value}"
+      path << " L #{i*space}  #{@dimensions[:height]-value} "
       i+=1
     end
-
-    path << "L #{@window_width} #{@window_height} Z"
+    path << "L #{@dimensions[:width]} #{@dimensions[:height]} Z"
   end
 end
