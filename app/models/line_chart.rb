223be3bd503ffd58c490ee7e707ea695f3c6b316
franzi <foberhauser.mmt-b2012@fh-salzburg.ac.at>
2013-12-26 21:07:59 +0100

    line chart with correct width and height

--- app/models/line_chart.rb
+++ app/models/line_chart.rb
@@ -7,24 +7,25 @@ class LineChart < ActiveRecord::Base
   end
 
   def build_paths(items)
+
     ingredients = {}
+
     items.each_with_index do |item, index|
       # multiplication factor 20 just for a better illustration
-      ingredients["kcal"] = (100/(2500 / item[:nutritions]["kcal"])) * 20
-      ingredients["fat"] =  (100/(92.5/item[:nutritions]["fat"])) * 20
-      ingredients["saturated_fat"] = (100/(16/item[:nutritions]["saturated_fat"])) * 20
-      ingredients["protein"] =  (100/(78/item[:nutritions]["protein"])) * 20
-      ingredients["sugar"] =  (100/(62.2/item[:nutritions]["sugar"])) * 20
-      ingredients["carbohydrate"] = (100/(313/item[:nutritions]["carbohydrate"])) * 20
-      ingredients["fiber"] =  (100/(15/item[:nutritions]["fiber"])) * 20
-      ingredients["sodium"] =  (100/(2400/item[:nutritions]["sodium"])) * 20
-      logger.debug(ingredients)
+      ingredients["kcal"] = (100/(2500 / item[:nutritions]["kcal"]).ceil) * 20
+      ingredients["fat"] =  (100/(92.5/item[:nutritions]["fat"]).ceil) * 20
+      ingredients["saturated_fat"] = (100/(16/item[:nutritions]["saturated_fat"]).ceil) * 20
+      ingredients["protein"] =  (100/(78/item[:nutritions]["protein"]).ceil) * 20
+      ingredients["sugar"] =  (100/(62.2/item[:nutritions]["sugar"]).ceil) * 20
+      ingredients["carbohydrate"] = (100/(313/item[:nutritions]["carbohydrate"]).ceil) * 20
+      ingredients["fiber"] =  (100/(15/item[:nutritions]["fiber"]).ceil) * 20
+      ingredients["sodium"] =  (100/(2400/item[:nutritions]["sodium"]).ceil) * 20
       item[:path] = draw_function ingredients
     end
   end
 
   def draw_function(ingredients)
-    space = 500/ingredients.length
+    space = @dimensions[:width]/ingredients.length
     path = "M 0 #{@dimensions[:height]}"
     i = 0
     ingredients.each do |key, value|
