abfdd9cd7376d6abbe0d9eed9f92d36ee2b7d8bf
franzi <foberhauser.mmt-b2012@fh-salzburg.ac.at>
2013-12-26 23:36:42 +0100

    line chart drawn with bd values

--- app/models/line_chart.rb
+++ app/models/line_chart.rb
@@ -12,7 +12,8 @@ class LineChart < ActiveRecord::Base
 
     items.each_with_index do |item, index|
       # multiplication factor 20 just for a better illustration
-      ingredients["kcal"] = (100/(2500 / item[:nutritions]["kcal"]).ceil) * 20
+
+      ingredients["kcal"] = (100/(2500/item[:nutritions]["kcal"]).ceil) * 20
       ingredients["fat"] =  (100/(92.5/item[:nutritions]["fat"]).ceil) * 20
       ingredients["saturated_fat"] = (100/(16/item[:nutritions]["saturated_fat"]).ceil) * 20
       ingredients["protein"] =  (100/(78/item[:nutritions]["protein"]).ceil) * 20
@@ -20,6 +21,7 @@ class LineChart < ActiveRecord::Base
       ingredients["carbohydrate"] = (100/(313/item[:nutritions]["carbohydrate"]).ceil) * 20
       ingredients["fiber"] =  (100/(15/item[:nutritions]["fiber"]).ceil) * 20
       ingredients["sodium"] =  (100/(2400/item[:nutritions]["sodium"]).ceil) * 20
+
       item[:path] = draw_function ingredients
     end
   end
@@ -31,6 +33,8 @@ class LineChart < ActiveRecord::Base
     ingredients.each do |key, value|
       path << " L #{i*space}  #{@dimensions[:height]-value} "
       i+=1
+      @values = key
+      logger.debug(@values)
     end
     path << "L #{@dimensions[:width]} #{@dimensions[:height]} Z"
   end
