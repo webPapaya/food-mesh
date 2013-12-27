af28c7f71b894145655795535db07ea5397476bf
franzi <foberhauser.mmt-b2012@fh-salzburg.ac.at>
2013-12-27 16:31:49 +0100

    line chart with extra function calculating the percentage

--- app/models/line_chart.rb
+++ app/models/line_chart.rb
@@ -7,15 +7,16 @@ class LineChart < ActiveRecord::Base
   end
 
   def convert_to_percent ingredients, items
+    # multiplication factor 15 just for a better illustration,
     items.each do |item, index|
-      ingredients["kcal"] = (100/(2500/ingredients["kcal"])) * 15
-      ingredients["fat"] =  (100/(92.5/ingredients["fat"])) * 15
-      ingredients["saturated_fat"] = (100/(16/ingredients["saturated_fat"])) * 15
-      ingredients["protein"] =  (100/(78/ingredients["protein"])) * 15
-      #ingredients["sugar"] =  (100/(62.2/ingredients["sugar"]).ceil) * 15
-      #ingredients["carbohydrate"] = (100/(313/ingredients["carbohydrate"]).to_i) * 15
-      ingredients["fiber"] =  (100/(15/ ingredients["fiber"])) * 15
-      #ingredients["sodium"] =  (100/(2400/ingredients["sodium"]).to_i) * 15
+      ingredients["kcal"] = (100/(2500/ingredients["kcal"]))
+      ingredients["fat"] =  (100/(92.5/ingredients["fat"]))
+      ingredients["saturated_fat"] = (100/(16/ingredients["saturated_fat"]))
+      ingredients["protein"] =  (100/(78/ingredients["protein"]))
+      ingredients["sugar"] =  (100/(62.2/ingredients["sugar"]))
+      ingredients["carbohydrate"] = (100/(313/ingredients["carbohydrate"]))
+      ingredients["fiber"] =  (100/(15/ ingredients["fiber"]))
+      ingredients["sodium"] =  (100/(2400/ingredients["sodium"]))
       #logger.debug(ingredients)
       #item[:path] = draw_function ingredients
     end
@@ -26,18 +27,20 @@ class LineChart < ActiveRecord::Base
     ingredients = {}
 
     items.each_with_index do |item, index|
-      # multiplication factor 15 just for a better illustration
+      # .to_f prevents from getting nil into line chart
 
-      ingredients["kcal"] = item[:nutritions]["kcal"]
-      ingredients["fat"] =  item[:nutritions]["fat"]
-      ingredients["saturated_fat"] = item[:nutritions]["saturated_fat"]
-      ingredients["protein"] =  item[:nutritions]["protein"]
-      ingredients["sugar"] = item[:nutritions]["sugar"]
-      ingredients["carbohydrate"] = item[:nutritions]["carbohydrate"]
-      ingredients["fiber"] = item[:nutritions]["fiber"]
-      ingredients["sodium"] = item[:nutritions]["sodium"]
+      ingredients["kcal"] = item[:nutritions]["kcal"].to_f
+      ingredients["fat"] =  item[:nutritions]["fat"].to_f
+      ingredients["saturated_fat"] = item[:nutritions]["saturated_fat"].to_f
+      ingredients["protein"] =  item[:nutritions]["protein"].to_f
+      ingredients["sugar"] = item[:nutritions]["sugar"].to_f
+      ingredients["carbohydrate"] = item[:nutritions]["carbohydrate"].to_f
+      ingredients["fiber"] = item[:nutritions]["fiber"].to_f
+      ingredients["sodium"] = item[:nutritions]["sodium"].to_f
       logger.debug(ingredients)
       convert_to_percent ingredients, items
+      item[:path] = draw_function ingredients
+
     end
 
   end
