46b70ff07ba72eccf32b2c84af300944889d0a39
franzi <foberhauser.mmt-b2012@fh-salzburg.ac.at>
2013-12-27 12:35:56 +0100

    detected negative values in data flow

--- app/models/line_chart.rb
+++ app/models/line_chart.rb
@@ -6,25 +6,42 @@ class LineChart < ActiveRecord::Base
     }
   end
 
+  def convert_to_percent ingredients, items
+    items.each do |item, index|
+      ingredients["kcal"] = (100/(2500/ingredients["kcal"])) * 15
+      ingredients["fat"] =  (100/(92.5/ingredients["fat"])) * 15
+      ingredients["saturated_fat"] = (100/(16/ingredients["saturated_fat"])) * 15
+      ingredients["protein"] =  (100/(78/ingredients["protein"])) * 15
+      #ingredients["sugar"] =  (100/(62.2/ingredients["sugar"]).ceil) * 15
+      #ingredients["carbohydrate"] = (100/(313/ingredients["carbohydrate"]).to_i) * 15
+      ingredients["fiber"] =  (100/(15/ ingredients["fiber"])) * 15
+      #ingredients["sodium"] =  (100/(2400/ingredients["sodium"]).to_i) * 15
+      #logger.debug(ingredients)
+      #item[:path] = draw_function ingredients
+    end
+  end
+
   def build_paths(items)
 
     ingredients = {}
 
     items.each_with_index do |item, index|
-      # multiplication factor 20 just for a better illustration
+      # multiplication factor 15 just for a better illustration
 
-      ingredients["kcal"] = (100/(2500/item[:nutritions]["kcal"]).ceil) * 20
-      ingredients["fat"] =  (100/(92.5/item[:nutritions]["fat"]).ceil) * 20
-      ingredients["saturated_fat"] = (100/(16/item[:nutritions]["saturated_fat"]).ceil) * 20
-      ingredients["protein"] =  (100/(78/item[:nutritions]["protein"]).ceil) * 20
-      ingredients["sugar"] =  (100/(62.2/item[:nutritions]["sugar"]).ceil) * 20
-      ingredients["carbohydrate"] = (100/(313/item[:nutritions]["carbohydrate"]).ceil) * 20
-      ingredients["fiber"] =  (100/(15/item[:nutritions]["fiber"]).ceil) * 20
-      ingredients["sodium"] =  (100/(2400/item[:nutritions]["sodium"]).ceil) * 20
-
-      item[:path] = draw_function ingredients
+      ingredients["kcal"] = item[:nutritions]["kcal"]
+      ingredients["fat"] =  item[:nutritions]["fat"]
+      ingredients["saturated_fat"] = item[:nutritions]["saturated_fat"]
+      ingredients["protein"] =  item[:nutritions]["protein"]
+      ingredients["sugar"] = item[:nutritions]["sugar"]
+      ingredients["carbohydrate"] = item[:nutritions]["carbohydrate"]
+      ingredients["fiber"] = item[:nutritions]["fiber"]
+      ingredients["sodium"] = item[:nutritions]["sodium"]
+      logger.debug(ingredients)
+      convert_to_percent ingredients, items
     end
+
   end
+
 
   def draw_function(ingredients)
     space = @dimensions[:width]/ingredients.length
