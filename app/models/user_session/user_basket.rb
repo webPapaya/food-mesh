725b2f3f6fb71914d41233116902966f46091d1f
thomas mayrhofer <thomas@mayrhofer.at>
2013-12-29 18:25:01 +0100

    removed bug with duplicated items in the user basket

--- app/models/user_session/user_basket.rb
+++ app/models/user_session/user_basket.rb
@@ -4,8 +4,11 @@ module UserBasket
     @session[:item_basket] = []
   end
 
+  ##
+  # checks if element is already in the user list
+  # if not it adds the item to the item_basket
   def add_item_to_basket(item_id)
-    @session[:item_basket] << item_id
+    @session[:item_basket] << item_id unless (@session[:item_basket].include? item_id)
   end
 
   def get_user_items
