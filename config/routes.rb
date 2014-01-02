46381679d766f92f6735cde35e6022c161ce5327
thomas mayrhofer <thomas@mayrhofer.at>
2014-01-02 23:20:49 +0100

    added compare view to food_item

--- config/routes.rb
+++ config/routes.rb
@@ -11,17 +11,19 @@ Food::Application.routes.draw do
       match '/:item_id', to: 'food_item#show', as: 'get_food_item', via: :get
       match '/add_to_basket/:item_id', to: 'user_session#add_items_to_basket', via: [:post, :get], as: 'add_item_to_basket'
       match '/delete_from_basket/:item_id', to: 'user_session#delete_item_from_basket', via: [:post, :get], as: 'delete_item_from_basket'
+      match '/delete_items_from_basket/', to: 'user_session#delete_all_items', via: [:post, :get], as: 'bulk_delete_basket'
     end
 
     #search
-    match 'search/:query', to: 'food_item#search', as: 'search_db', via: :get
+    match '/search/:query', to: 'food_item#search', as: 'search_db', via: :get
+    match '/compare', to: 'food_item#compare', as: 'compare_items', via: :get
   end
 
-  match 'change/url/to/:locale(/:query)', to: 'application#change_lang', via: [:get, :post], as: 'change_lang'
+  match '/change/url/to/:locale(/:query)', to: 'application#change_lang', via: [:get, :post], as: 'change_lang'
 
 
   #user session
-  match 'user_session/change_user_settings', to: 'user_session#change_user_settings', :via => [:post]
+  match '/user_session/change_user_settings', to: 'user_session#change_user_settings', :via => [:post]
   resources :line_charts
 
 
