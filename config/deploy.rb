6d32bcdc6aec55bc7d4bcf15ee54f853b75535e4
thomas mayrhofer <thomas@mayrhofer.at>
2014-01-02 13:47:48 +0100

    changed deploy.rb to right settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :branch, "master"
 #
 set :use_sudo, false
 #
-set :deploy_to, "/var/www/virthosts/eight"
+set :deploy_to, "/var/www/virthosts/foodmesh.mediacube.at"
 #
 role :web, "rails.multimediatechnology.at:5412"                          # Your HTTP server, Apache/etc
 role :app, "rails.multimediatechnology.at:5412"                          # This may be the same as your `Web` server
@@ -28,29 +28,3 @@ namespace :deploy do
     end
   end
 end
-
-
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
-
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-role :web, "rails.mediacube.at"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
