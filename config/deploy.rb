55453bdb5ce2b58a1dddd0bb0114b0c32a62da5f
thomas mayrhofer <thomas@mayrhofer.at>
2014-01-02 13:57:04 +0100

    added assets role to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,16 +6,19 @@ set :rvm_type, :system
 set :application, "Food Mesh"
 set :repository,  "git@git.mediacube.at:fhs34784/food-mesh.git"
 set :scm, :git
-set :user, "deploy_eight"
+set :user, "deploy_foodmesh"
 set :branch, "master"
+set :assets_role, :app
 #
 set :use_sudo, false
 #
-set :deploy_to, "/var/www/virthosts/eight"
+set :deploy_to, "/var/www/virthosts/foodmesh.mediacube.at"
 #
 role :web, "rails.multimediatechnology.at:5412"                          # Your HTTP server, Apache/etc
 role :app, "rails.multimediatechnology.at:5412"                          # This may be the same as your `Web` server
 role :db,  "rails.multimediatechnology.at:5412", :primary => true # This is where Rails migrations will run
+
+
 #
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
