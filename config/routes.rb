ce8ed666b47d8a1c11746273327ca587b1f15e48
thomas mayrhofer <thomas@mayrhofer.at>
2014-01-03 18:26:02 +0100

    changed /login path to /admin

--- config/routes.rb
+++ config/routes.rb
@@ -35,8 +35,8 @@ Food::Application.routes.draw do
   resources 'basic_pages'
 
   #admin login
-  get "logout" => "sessions#destroy", :as => "logout"
-  get "login" => "sessions#new", :as => "login"
+  get '/logout' => "sessions#destroy", :as => "logout"
+  get '/admin' => "sessions#new", :as => "login"
   get "signup" => "users#new", :as => "signup"
   resources :users
   resources :sessions
