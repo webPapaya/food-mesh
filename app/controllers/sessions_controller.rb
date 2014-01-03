73d43395bf18c7878aace66430da7fee4f4b6646
franzi <foberhauser.mmt-b2012@fh-salzburg.ac.at>
2014-01-03 14:53:00 +0100

    seperate administration page for backend modifications

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = login(params[:email], params[:password], params[:remember_me])
     if user
-      redirect_back_or_to root_url, :notice => "Logged in!"
+      redirect_to signup_path, :notice => "Logged in!"
     else
       flash.now.alert = "Email or password was invalid"
       render :new
@@ -11,6 +11,6 @@ class SessionsController < ApplicationController
 
   def destroy
     logout
-    redirect_to root_url, :notice => "Logged out!"
+    redirect_to root_url
   end
 end
