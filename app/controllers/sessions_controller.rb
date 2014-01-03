d4f06d6b9e1bb84af534aa6a1294973e95bde313
thomas mayrhofer <thomas@mayrhofer.at>
2014-01-03 19:14:40 +0100

    added redirect to admin_dashboard on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   def create
     user = login(params[:email], params[:password], params[:remember_me])
     if user
-      redirect_to signup_path, :notice => "Logged in!"
+      redirect_to dashboard_path, :notice => "Logged in!"
     else
       flash.now.alert = "Email or password was invalid"
       render :new
