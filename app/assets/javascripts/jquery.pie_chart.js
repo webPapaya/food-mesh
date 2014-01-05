7fef0d140c6779350cc9473da2d94365b56780b9
thomas mayrhofer <thomas@mayrhofer.at>
2014-01-06 00:14:09 +0100

    added loading animation for pie-chart

--- app/assets/javascripts/jquery.pie_chart.js
+++ app/assets/javascripts/jquery.pie_chart.js
@@ -15,19 +15,32 @@
 (function($, d3) {
     var PieChart = function(element, options) {
         this.element = element;
+
         this.$svg = d3.select('#' + this.element.attr('id'));
 
         this.$element = $(element);
         this.$pieElements = this.$element.find('.pie-item');
         this.$pieItems = this.$element.find('.pie-path-back');
 
+
+
+        this.loadingAnimation();
+
         if(options.controll) {
             this.$controlles = $(options.controll);
             this.$controlItems = this.$controlles.find('.col');
             this.$controlItems.on('mouseenter',this.highLightPie.bind(this));
         }
+    };
 
-        this.$pieItems.hoverIntent(this.mouseOverEvent.bind(this),  this.mouseLeaveEvent.bind(this));
+    PieChart.prototype.loadingAnimation = function() {
+        this.$element.css('display', '');
+        var elements = this.$svg.selectAll('.clip-path').each(function(idx, element){
+            var element = d3.select(this);
+            var radius = element.attr('r');
+            element.attr('r', 0);
+            element.transition().duration(1000).attr('r', radius).ease('bounce');
+        });
     };
 
     PieChart.prototype.highLightPie = function(evt) {
@@ -36,8 +49,7 @@
         var clipPath = this.$svg.select('#' + dataReference + ' .clip-path');
         var originalRadius = clipPath.attr('r');
 
-        clipPath.transition().attr('r', 0);
-
+        clipPath.transition().attr('r', originalRadius * 1.1).ease('bounce');
 
         $(evt.currentTarget).on('mouseleave', (function(evt){
             clipPath.transition().attr('r', originalRadius);
@@ -51,29 +63,31 @@
         });
     };
 
-    PieChart.prototype.mouseOverEvent = function(evt) {
-        var $currentTarget = $(evt.currentTarget);
-        var $parrentWrapper = $currentTarget.closest('g');
-        var $clipPath = $parrentWrapper.find('clippath circle');
-        var $text = $parrentWrapper.children('text');
 
-
-        this.hideAllLabels($parrentWrapper.attr('id'));
-
-        $text.animate({
-            opacity: 1
-        }, {queue: false});
-
-        $clipPath.animate({
-            r: 0
-        }, {queue: false});
-    };
-
-    PieChart.prototype.hideAllLabels = function(id) {
-        this.$pieElements.not('#' + id).children('text').animate({
-            opacity: 0.1
-        });
-    };
+//currently not used
+//    PieChart.prototype.mouseOverEvent = function(evt) {
+//        var $currentTarget = $(evt.currentTarget);
+//        var $parrentWrapper = $currentTarget.closest('g');
+//        var $clipPath = $parrentWrapper.find('clippath circle');
+//        var $text = $parrentWrapper.children('text');
+//
+//
+//        this.hideAllLabels($parrentWrapper.attr('id'));
+//
+//        $text.animate({
+//            opacity: 1
+//        }, {queue: false});
+//
+//        $clipPath.animate({
+//            r: 0
+//        }, {queue: false});
+//    };
+//
+//    PieChart.prototype.hideAllLabels = function(id) {
+//        this.$pieElements.not('#' + id).children('text').animate({
+//            opacity: 0.1
+//        });
+//    };
 
     $.fn.PieChart = function(options) {
         return this.each(function() {
