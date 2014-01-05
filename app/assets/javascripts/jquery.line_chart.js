9fa4aff0dd5f798b3aa29bcfca207b72018282c3
franzi <foberhauser.mmt-b2012@fh-salzburg.ac.at>
2014-01-05 15:39:28 +0100

    animated line chart with jQuery

--- app/assets/javascripts/jquery.line_chart.js
+++ app/assets/javascripts/jquery.line_chart.js
@@ -1,14 +1,39 @@
-
-
 (function($) {
     var LineChart = function(element, options) {
+        console.log(element)
+        this.$element = $(element);
+        this.$paths = this.$element.find('.line-chart-line');
 
+        this.$paths.on("mouseover", (function(evt){
+            console.log(evt);
 
+        }).bind(this))
 
+        for(path in this.$paths.get()) {
+            this.animatePath(this.$paths[path]);
+        }
 
 
     };
 
+    LineChart.prototype.animatePath = function(path) {
+        var length = path.getTotalLength();
+
+        // clear previous transition
+        path.style.transition = path.style.WebkitTransition = 'none';
+
+        // starting positions
+        path.style.strokeDasharray = length + ' ' + length;
+        path.style.strokeDashoffset = length;
+
+        // trigger a layout
+        path.getBoundingClientRect();
+
+        // define transition
+        path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 2s ease-in-out';
+        path.style.strokeDashoffset = '0';
+
+    };
 
     $.fn.LineChart = function(options) {
         return this.each(function() {
