b4ea97acec87a14cd2ded84995d8fa3c90284e3f
thomas mayrhofer <thomas@mayrhofer.at>
2014-02-01 19:18:49 +0100

    added tests for food_item controller

--- app/models/food_apis_module.rb
+++ app/models/food_apis_module.rb
@@ -16,7 +16,7 @@ require_dependency 'food_apis/nutritionix_api'
 module FoodApisModule
   @@apis = [NutritionixAPI.new, Fddb.new, Fsecret.new]
 
-  ##
+
   # example return value for search
   #  [{
   #      "name"=>"Tilapia (Fish)",
--- test/controllers/food_item_controller_test.rb
+++ test/controllers/food_item_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class FoodItemControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'should get show' do
+    get :show, :item_id => '0-513fceb475b8dbbc21000fd4', :locale => 'en'
+    assert_response :success
+  end
+
+  test 'should get search' do
+    get :search, :query => 'banana', :locale => 'en'
+    assert_response :success
+  end
 end
